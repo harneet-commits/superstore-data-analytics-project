7c8b37b7a301b8f4c0d1d09d3cd9e25fe949e315
harneet-commits <learningharneet@gmail.com>
2026-06-05 13:03:48 +0530

    Add files via upload

---
+++ Superstore SQL File.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE cleaned_sales_data;
+USE cleaned_sales_data;
+CREATE TABLE sales (
+    order_id VARCHAR(50),
+    order_date DATE,
+    customer_name VARCHAR(100),
+    region VARCHAR(50),
+    category VARCHAR(50),
+    sales DECIMAL(10,2),
+    quantity INT,
+    profit DECIMAL(10,2),
+    discount DECIMAL(5,2)
+);
